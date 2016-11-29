ae968ce6efe1324eaba3e7f8a78173ebe01b0c4f
clizaga <clizaga@hiberus.com>
2016-11-29 22:52:59 +0100

    stuff

--- dwes/tablaMultiplicar.php
+++ dwes/tablaMultiplicar.php
@@ -1,21 +1,19 @@
 <?php
-$numero = floor(rand(1,100));
-$tabla = array(0,1,2,3,4,5,6,7,8,9,10);
+$numero = floor(rand(1, 100));
+$tabla = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<ul>
-<?php
-    echo '<li>Tabla del '.$numero;
-    foreach ($tabla as $fila){
-        echo '<li>'.$numero.' por '.$fila.' = '.$numero*$fila.'</li>';
-    }
-?>
-</ul>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+        <ul>
+            <li>Tabla del <?php echo $numero ?></li>
+            <?php foreach ($tabla as $fila): ?>
+                <li><?php echo $numero ?> por <?php echo $fila ?> = <?php echo $numero * $fila ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
 </html>
