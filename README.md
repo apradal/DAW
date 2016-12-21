7087d8957414d26ab3d8baea0a135c4147df3e31
antonio <antoniopradalara@gmail.com>
2016-12-21 20:58:01 +0100

    testing :D

--- README.md
+++
@@ -1,2 +0,0 @@
-# DAW
-Ejercicios de segundo curso daw
--- dwes/formularios/datosUsuario.php
+++ dwes/formularios/datosUsuario.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <style>
-        .error {color: red};
+        .error {color: orange};
     </style>
 </head>
 <body>
