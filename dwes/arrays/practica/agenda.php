40431570f9cb44445b4944b819534fc960beac66
Victor <vjurado@hiberus.com>
2016-12-30 19:56:55 +0100

    Agenda improvement

--- dwes/arrays/practica/agenda.php
+++ dwes/arrays/practica/agenda.php
@@ -4,42 +4,49 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <style>
-        body{
+        body {
             padding: 0;
             margin: 0;
             background-color: #ffffff;
         }
+
         .error {
             color: red;
             display: block;
         }
-        #display{
+
+        #display {
             max-width: 900px;
             margin: auto;
             background-color: #EBE8DE;
         }
-        #display h1{
+
+        #display h1 {
             background-color: black;
             margin: auto;
             text-align: center;
             color: #E56038;
             font-size: 2em;
         }
-        .contactos{
+
+        .contactos {
             display: inline-block;
             margin-left: 100px;
             width: 300px;
         }
-        .contactos h3{
+
+        .contactos h3 {
             color: #000000;
             font-size: 1.5em;
             border-bottom: 1px solid black;
         }
-        .contactos ul{
+
+        .contactos ul {
             list-style: none;
             margin-left: -40px;
         }
-        #form{
+
+        #form {
             max-width: 900px;
             margin: auto;
             background-color: #EBE8DE;
@@ -63,17 +70,15 @@ Si el nombre que se introdujo ya existe en la agenda y se indica un número de t
 Si el nombre que se introdujo ya existe en la agenda y no se indica número de teléfono,
  se eliminará de la agenda la entrada correspondiente a ese nombre.
 */
-//Sanitizo los datos recibidos
-$contact = filter_input(INPUT_POST, 'contact', FILTER_SANITIZE_STRING);
-$contact = strtolower($contact);
-$phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_NUMBER_INT);
+
 /**
  * Imprime los contactos del array
  * @param $array
  */
-function printArrayContact($array){
-    foreach ($array as $key => $value){
-        echo '<li>'.$key.'</li><br/>';
+function printArrayContact($array)
+{
+    foreach ($array as $key => $value) {
+        echo '<li>' . $key . '</li><br/>';
     }
 }
 
@@ -81,41 +86,53 @@ function printArrayContact($array){
  * Imprime los telefonos de los contactos del array
  * @param $array
  */
-function printArrayPhones($array){
-    foreach ($array as $key => $value){
-        echo '<li>'.$value.'</li><br/>';
+function printArrayPhones($array)
+{
+    foreach ($array as $key => $value) {
+        echo '<li>' . $value . '</li><br/>';
     }
 }
-function checkContacts($contacts, $contact){
-    $exists = false;
+
+function checkContacts($contacts, $contact)
+{
     foreach ($contacts as $key) {
-        if ($key == $contact) $exists = true;
+        if ($key == $contact) {
+            return true;
+        }
     }
-    return $exists;
+
+    return false;
 }
+
 ?>
 <section id="display">
     <h1>Agenda</h1>
     <?php
-    if ($_POST['add'] && empty($contact)) {
-        echo '<span class="error">Error: el contacto no puede estar vacio</span>';
-        $contacts = $_POST['contacts'];
-        $phones = $_POST['phones'];
-        for ($i = 0; $i < count($contacts);$i++){
-            $contactsMult[$contacts[$i]] = $phones[$i];
-        }
-    } else if ($_POST['add'] && !empty($contact)) {
-        $contacts = $_POST['contacts'];
-        $phones = $_POST['phones'];
-        for ($i = 0; $i < count($contacts);$i++){
-            $contactsMult[$contacts[$i]] = $phones[$i];
-        }
-        if (!checkContacts($contacts, $contact) && (!empty($phone) || $phone === '0')){
-            $contactsMult[$contact] = $phone;
-        } else if(checkContacts($contacts, $contact) && (!empty($phone) || $phone === '0')){
-            $contactsMult[$contact] = $phone;
-        } else if (checkContacts($contacts, $contact) && empty($phone)){
-            unset($contactsMult[$contact]);
+    //Sanitizo los datos recibidos
+    $contact = isset($_POST['contact']) ? strtolower(filter_input(INPUT_POST, 'contact', FILTER_SANITIZE_STRING)) : null;
+    $phone = isset($_POST['phone']) ? filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_NUMBER_INT) : null;
+    $contactsMult = array();
+
+    if (isset($_POST['add'])) {
+        $contacts = isset($_POST['contacts']) ? $_POST['contacts'] : array();
+        $phones = isset($_POST['phones']) ? $_POST['phones'] : array();
+
+        if (empty($contact)) {
+            $error = 'Error: el contacto no puede estar vacio';
+            for ($i = 0; $i < count($contacts); $i++) {
+                $contactsMult[$contacts[$i]] = $phones[$i];
+            }
+        } else {
+            for ($i = 0; $i < count($contacts); $i++) {
+                $contactsMult[$contacts[$i]] = $phones[$i];
+            }
+            if (!checkContacts($contacts, $contact) && (!empty($phone) || $phone === '0')) {
+                $contactsMult[$contact] = $phone;
+            } else if (checkContacts($contacts, $contact) && (!empty($phone) || $phone === '0')) {
+                $contactsMult[$contact] = $phone;
+            } else if (checkContacts($contacts, $contact) && empty($phone)) {
+                unset($contactsMult[$contact]);
+            }
         }
     }
     ?>
@@ -137,17 +154,18 @@ function checkContacts($contacts, $contact){
     </article>
 </section>
 <section id="form">
-    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
         <fieldset>
             <legend><b>Añadir contacto</b></legend>
+            <?php if (isset($error)) : ?>
+                <span class="error"><?php echo $error ?></span>
+            <?php endif ?>
             <label for="contacto">Contacto: <input type="text" name="contact" id="contact"></label>
             <label for="phone">Teléfono: <input type="number" name="phone" id="phone"></label>
-            <?php
-            foreach ($contactsMult as $key => $value){
-                echo '<input type="hidden" value="'.$key.'" name="contacts[]"/>';
-                echo '<input type="hidden" value="'.$value.'" name="phones[]"/>';
-            }
-            ?>
+            <?php foreach ($contactsMult as $key => $value) : ?>
+                <input type="hidden" value="<?php echo $key ?>" name="contacts[]"/>
+                <input type="hidden" value="<?php echo $value ?>" name="phones[]"/>
+            <?php endforeach ?>
             <input type="submit" value="Añadir" name="add"/>
         </fieldset>
     </form>
